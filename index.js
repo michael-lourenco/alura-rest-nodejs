6d3c5b213a79d7a4b4e5295284c2e541ac5117ad
MichaelLourenco <michael.lourenco@pixter.com.br>
2022-01-13 23:02:20 -0300

    Criada rota atendimentos

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 
 app.listen(3000, ()=> console.log('Servidor rodando na porta 3000'))
 
-app.get('/', (req, res) => res.send('Servidor rodando, tudo ok'))
+app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos'))
