2bc0ebf744d8a824e14811ac56689cf34af826c6
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-01-18 20:21:57 -0300

    Status HTTP

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,7 +4,6 @@ module.exports = app => {
   app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
  
   app.post('/atendimentos', (req, res) => {
-    Atendimento.adiciona(req.body)
-    res.send('Você está na rota de atendimentos e está realizando um POST')
+    Atendimento.adiciona(req.body, res)
   })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,19 +2,19 @@ const moment = require('moment');
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
         const data = moment(atendimento.data, 'DD-MM-YYYY').format('YYYY-MM-DD HH:mm:ss')
         const atendimentoDatado = {...atendimento, dataCriacao, data}
-        return new Promise((resolve, reject) => {
-            let sql = `INSERT INTO Atendimentos SET ?`;            
-            conexao.query(sql, atendimentoDatado, (err, resultados) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(resultados);
-                }
-            });
+ 
+        const sql = `INSERT INTO Atendimentos SET ?`;
+
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(201).json(resultados);
+            }
         });
     }
 }
