0a9d5ad6182160491a0c3e0ad50552bf65b77dd2
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-01-18 20:32:48 -0300

    Aplicando validações de entrada

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,17 +5,45 @@ class Atendimento {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
         const data = moment(atendimento.data, 'DD-MM-YYYY').format('YYYY-MM-DD HH:mm:ss')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
- 
-        const sql = `INSERT INTO Atendimentos SET ?`;
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if (erro) {
-                res.status(400).json(erro)
-            } else {
-                res.status(201).json(resultados);
+        const clienteEhValido = atendimento.cliente.length >= 5
+        
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter no mínimo 5 caracteres'
             }
-        });
+        ]
+
+
+
+        const erros = validacoes.filter(campo => !campo.valido)
+
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+    
+            const sql = `INSERT INTO Atendimentos SET ?`;
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados);
+                }
+            });
+        }
     }
 }
 
