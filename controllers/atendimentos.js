b142b188378c2dedaef8e4206d9f0aee7dd2d946
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-01-18 21:06:06 -0300

    UPDATE

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -13,4 +13,10 @@ module.exports = app => {
   app.post('/atendimentos', (req, res) => {
     Atendimento.adiciona(req.body, res)
   })
+
+  app.patch('/atendimentos/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const valores = req.body
+    Atendimento.altera(id, valores, res)
+  })
 }
