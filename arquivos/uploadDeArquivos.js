e15425720e6acb9dab846e7f72edebc68bc08522
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-01-18 22:12:37 -0300

    Buffer

---
+++ arquivos/uploadDeArquivos.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+fs.readFile('./assets/salsicha.jpg', (erro, buffer) => {
+    console.log('imagem foi bufferizada')
+    fs.writeFile('./assets/salsicha2.jpg', buffer, (erro) => {
+        console.log('imagem foi escrita')
+    })
+})
