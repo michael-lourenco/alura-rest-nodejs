e505e2325e0fa94d1a0a928c1dfd2b702b121d46
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-01-18 21:15:22 -0300

    DELETE

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -40,7 +40,7 @@ class Atendimento {
                 if (erro) {
                     res.status(400).json(erro)
                 } else {
-                    res.status(201).json(resultados);
+                    res.status(201).json(atendimento);
                 }
             });
         }
@@ -78,10 +78,22 @@ class Atendimento {
             if(erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(200).json(resultados)
+                res.status(200).json({...valores, id})
             }
         })
     }
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id = ?'
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
+    }
+
 }
 
 module.exports = new Atendimento
