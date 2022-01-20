ea16c38bc9fe9c20af53f727448ad99f20cf964c
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-01-20 20:15:43 -0300

    Segurança ao receber dados externos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,11 +1,24 @@
 const fs = require('fs')
+const path = require('path')
 
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => { 
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
 
-    fs.createReadStream(caminho)
-        .pipe(fs.createWriteStream(novoCaminho))
-        .on('finish', () => callbackImagemCriada(novoCaminho))
+    const tiposValidos = ['png', 'jpeg', 'jpg', 'gif']
+
+    const tipo = path.extname(caminho)
+
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+
+    if(tipoEhValido === -1) {
+        callbackImagemCriada('Tipo de arquivo inválido')
+        return
+    } else {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+    
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(novoCaminho))
+    }
 }
 
