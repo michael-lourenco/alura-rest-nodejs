96009712d6e582b345fb9a47a8133b869e996d3d
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-01-22 19:12:14 -0300

    Refatorar validação

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -4,28 +4,43 @@ const conexao = require('../infraestrutura/database/conexao')
 const repositorio = require('../repositorios/atendimento')
 
 class Atendimento {
-    adiciona(atendimento) {
-        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
-        const data = moment(atendimento.data, 'DD-MM-YYYY').format('YYYY-MM-DD HH:mm:ss')
-        
-        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+    constructor() {
+        this.dataEhValida = ({data, dataCriacao}) => moment(data).isSameOrAfter(dataCriacao)
 
-        const clienteEhValido = atendimento.cliente.length >= 5
-        
-        const validacoes = [
+        this.clienteEhValido = (tamanho) => tamanho >= 5
+
+        this.valida = (parametros) => {
+            this.validacoes.filter(campo =>{
+                const { nome } = campo
+                const  parametro = parametros[nome]
+                return !campo.valido(parametro)
+            })
+        }
+
+        this.validacoes = [
             {
                 nome: 'data',
-                valido: dataEhValida,
+                valido: this.dataEhValida,
                 mensagem: 'Data deve ser maior ou igual a data atual'
             },
             {
                 nome: 'cliente',
-                valido: clienteEhValido,
+                valido: this.clienteEhValido,
                 mensagem: 'Cliente deve ter no mínimo 5 caracteres'
             }
         ]
+    }
 
-        const erros = validacoes.filter(campo => !campo.valido)
+    adiciona(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const data = moment(atendimento.data, 'DD-MM-YYYY').format('YYYY-MM-DD HH:mm:ss')
+        
+        const parametros = {
+            data: {data, dataCriacao},
+            cliente: { tamanho: atendimento.cliente.length }
+        }
+
+        const erros = this.valida(parametros)
 
         const existemErros = erros.length
 
@@ -33,7 +48,6 @@ class Atendimento {
             return new Promise((resolve, reject) => reject(erros))
         } else {
             const atendimentoDatado = {...atendimento, dataCriacao, data}
-    
             return repositorio.adiciona(atendimentoDatado)
                 .then(resultados => {
                     const id = resultados.insertId
